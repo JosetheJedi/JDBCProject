c27256ab67c388394fdc3ffeee962f66c5e79787
Jose Hernandez <Jose Hernandez>
2017-03-10 14:11:15 -0800

    Added Book and Publisher java beans

---
+++ src/beans/Publisher.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author Josef
+ */
+public class Publisher {
+    private String pName, pAddress, pPhone, pEmail;
+
+    public String getpName() {
+        return pName;
+    }
+
+    public String getpAddress() {
+        return pAddress;
+    }
+
+    public String getpPhone() {
+        return pPhone;
+    }
+
+    public String getpEmail() {
+        return pEmail;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public void setpAddress(String pAddress) {
+        this.pAddress = pAddress;
+    }
+
+    public void setpPhone(String pPhone) {
+        this.pPhone = pPhone;
+    }
+
+    public void setpEmail(String pEmail) {
+        this.pEmail = pEmail;
+    }
+    
+    
+}
