2fc8fa2471f741caad91ea03af124c85771f4ec5
Darren <dxddarren@gmail.com>
2017-03-10 15:01:30 -0800

    Added addBooks

--- src/tables/Books.java
+++ src/tables/Books.java
@@ -2,6 +2,7 @@
 package tables;
 
 import java.sql.*;
+import beans.Book;
 
 public class Books {
     
@@ -22,4 +23,39 @@ public class Books {
             System.err.println(ex);
         }
     }
+    
+    public static void displayAllInfo(Connection conn, String bTitle){
+        //TODO FINISH
+        String sql = "SELECT * FROM books WHERE booktitle = ?"; 
+        String bookTitle, groupName, pubName, yearPub;
+        int pages = 0;
+        
+        System.out.println("Retrieving all info for " + bTitle);
+        ResultSet rs = null;
+        try(PreparedStatement stmt = conn.prepareStatement(sql,
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_READ_ONLY);){
+            
+            stmt.setString(1, bTitle);
+            rs = stmt.executeQuery(sql);
+        }
+        catch (SQLException ex){
+            System.err.println(ex);
+        }
+    }
+    public static void addBook(Connection conn, Book insert){
+        //('GroupName', 'BookTitle', 'PubName', yearPub, numPages)
+        String sql = "INSERT INTO books VALUES(?,?,?,?,?)";
+        try(PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, insert.getGroupName());
+            stmt.setString(2, insert.getBookTitle());
+            stmt.setString(3, insert.getPublisherName());
+            stmt.setInt(4, insert.getYearpublished());
+            stmt.setInt(5, insert.getNumberofpages());
+            stmt.executeUpdate(sql);
+        }catch(SQLException ex){
+            System.err.println(ex);
+        }
+    }
+    
 }
