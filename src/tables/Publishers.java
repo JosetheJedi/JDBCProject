4f930f59a3f91993269d6265a2447e4c0a5b8274
Jose Hernandez <Jose Hernandez>
2017-03-10 16:43:44 -0800

    Added display all info functionality to publishers

--- src/tables/Publishers.java
+++ src/tables/Publishers.java
@@ -6,6 +6,7 @@
 package tables;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -42,5 +43,49 @@ public class Publishers {
     
     public static void displayAllInfo(Connection conn, String pName){
         
+        String sql = "SELECT * FROM publishers NATURAL JOIN books WHERE publishername = ?";
+        
+        ResultSet rs = null;
+        
+        
+        System.out.println("\nRetrieving all info for " + pName);
+        
+        String groupname, bookTitle, publisherName, pAddress, pPhone, pEmail;
+        int yearpublished = 0, numberofpages = 0;
+        
+        try(PreparedStatement stmt = conn.prepareStatement(sql, 
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_READ_ONLY);){
+            
+            stmt.setString(1, pName);
+            
+            rs = stmt.executeQuery();
+            
+            System.out.println("groupname, publisher name"
+                    + ", yearformed, subject, booktitle, publishername");
+            
+            if(!rs.next()){
+                System.err.println("NO SUCH GROUP IN DATABASE!");
+            }
+            else
+                rs.beforeFirst();
+            
+            while(rs.next()){
+                groupname = rs.getString("groupname");
+                bookTitle = rs.getString("booktitle");
+                publisherName = rs.getString("publishername");
+                pAddress = rs.getString("publisheraddress");
+                pPhone = rs.getString("publisherphone");
+                pEmail = rs.getString("publisheremail");
+                yearpublished = rs.getInt("yearpublished");
+                numberofpages = rs.getInt("numberofpages");
+                
+                System.out.println(groupname + " " + bookTitle + " " + 
+                        publisherName +  " " +pAddress +  " " +pPhone+ " " +
+                        pEmail + " " + yearpublished + " " + numberofpages);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
     }
 }
