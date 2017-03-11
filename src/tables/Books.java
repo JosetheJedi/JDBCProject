feca3bad5b563fdd37b748f5348245a7636c10b9
Darren <dxddarren@gmail.com>
2017-03-11 15:48:49 -0800

    specified book info function

--- src/tables/Books.java
+++ src/tables/Books.java
@@ -26,8 +26,10 @@ public class Books {
     
     public static void displayAllInfo(Connection conn, String bTitle){
         //TODO FINISH
-        String sql = "SELECT * FROM books WHERE booktitle = ?"; 
-        String bookTitle, groupName, pubName, yearPub;
+        String sql = "SELECT * FROM books NATURAL JOIN writinggroups "
+                + "NATURAL JOIN publishers WHERE booktitle = ?"; 
+        String bookTitle, groupName, pubName, yearPub, pubAddress, pubPhone, pubMail;
+        String headWriter, yearFormed, subject;
         int pages = 0;
         
         System.out.println("Retrieving all info for " + bTitle);
@@ -35,9 +37,33 @@ public class Books {
         try(PreparedStatement stmt = conn.prepareStatement(sql,
                 ResultSet.TYPE_SCROLL_INSENSITIVE,
                 ResultSet.CONCUR_READ_ONLY);){
-            
             stmt.setString(1, bTitle);
-            rs = stmt.executeQuery(sql);
+            rs = stmt.executeQuery();
+            if(!rs.next()){
+                System.err.println("No such book");
+            }
+            else{
+                rs.beforeFirst();
+            }
+                System.out.println("Title, Group Name, Head Writer, Year Formed,"
+                        + " Subject, Pages, Publisher, Year Published, Publisher Address");
+            while(rs.next()){
+                bookTitle = rs.getString("booktitle");
+                groupName = rs.getString("groupname");
+                pubName = rs.getString("publishername");
+                yearPub = rs.getString("yearpublished");
+                pages = rs.getInt("numberofpages");
+                pubAddress = rs.getString("publisheraddress");
+                pubPhone = rs.getString("publisherphone");
+                pubMail = rs.getString("publisheremail");
+                headWriter = rs.getString("headwriter");
+                yearFormed = rs.getString("yearformed");
+                subject = rs.getString("subject");
+                System.out.println(bookTitle + ", " + groupName + ", " + headWriter
+                                + ", " + yearFormed + ", " + subject + ", " + pages
+                                + ", " + pubName + ", " + yearPub + ", " + pubAddress
+                                + ", " + pubPhone + ", " + pubMail);
+            }
         }
         catch (SQLException ex){
             System.err.println(ex);
