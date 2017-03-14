8da71f869fa2b7c2f12f37c260ca0752f6b101b9
Darren <dxddarren@gmail.com>
2017-03-14 14:33:45 -0700

    Document writinggroups.java class

--- src/tables/WritingGroups.java
+++ src/tables/WritingGroups.java
@@ -1,76 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tables;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author Josef
- */
 public class WritingGroups {
-    
-    public static void displayAllGroups(Connection conn){
-        
+    /**
+     * displayAllGroups will display all the information on
+     * the writing groups listed in the table
+     * @param conn 
+     */
+    public static void displayAllGroups(Connection conn){       
         System.out.println("Retrieving all Writing Groups");
-        
         String sql = "SELECT groupName FROM WRITINGGROUPS";
         ResultSet rs = null;
-        try(Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);){
-            
+        try(Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);){    
             String groupName;
             rs = stmt.executeQuery(sql);
-            
             System.out.println("Group Name");
+            //will print the group name and its rows
             while(rs.next()){
                 groupName = rs.getString("groupName");
-                
                 System.out.println(rs.getRow() + ") " + groupName);
-            }
-            
-            
+            }                        
         } catch (SQLException ex) {
             System.err.println(ex);
         }
     }
-    
+    /**
+     * displayAllInfo will display all the information based on the
+     * given groupName given and the books associated with them
+     * @param conn
+     * @param gName String, group name to display the information 
+     */
     public static void displayAllInfo(Connection conn, String gName){
-        
         String sql = "SELECT * FROM writinggroups NATURAL JOIN books WHERE groupname = ?";
-        
-        ResultSet rs = null;
-        
-        
-        System.out.println("\nRetrieving all info for " + gName);
-        
+        ResultSet rs = null;        
+        System.out.println("\nRetrieving all info for " + gName);        
         String groupname, headwriter, yearformed, subject, booktitle, publishername;
         int yearpublished = 0, numberofpages = 0;
         
         try(PreparedStatement stmt = conn.prepareStatement(sql, 
                 ResultSet.TYPE_SCROLL_INSENSITIVE,
                 ResultSet.CONCUR_READ_ONLY);){
-            
-            stmt.setString(1, gName);
-            
+                stmt.setString(1, gName);            
             rs = stmt.executeQuery();
             
             System.out.println("groupname, headwriter, yearformed, subject, booktitle, publishername");
-            
             if(!rs.next()){
                 System.err.println("NO SUCH GROUP IN DATABASE!");
             }
             else
                 rs.beforeFirst();
-            
+            //prints all the information of the writing group
             while(rs.next()){
                 groupname = rs.getString("groupname");
                 headwriter = rs.getString("headwriter");
