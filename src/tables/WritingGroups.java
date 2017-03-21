769f9fb73c4d82bf5fd3b226d457c557d26962e6
Jose Hernandez-Uribe <Josefhu15@gmail.com>
2017-03-20 20:18:55 -0700

    fixed output header for writinggroups info

--- src/tables/WritingGroups.java
+++ src/tables/WritingGroups.java
@@ -1,4 +1,5 @@
 package tables;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,58 +7,62 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class WritingGroups {
+
     /**
-     * displayAllGroups will display all the information on
-     * the writing groups listed in the table
-     * @param conn 
+     * displayAllGroups will display all the information on the writing groups
+     * listed in the table
+     *
+     * @param conn
      */
-    public static void displayAllGroups(Connection conn){       
+    public static void displayAllGroups(Connection conn) {
         System.out.println("Retrieving all Writing Groups");
         String sql = "SELECT groupName FROM WRITINGGROUPS";
         ResultSet rs = null;
-        try(Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);){    
+        try (Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);) {
             String groupName;
             rs = stmt.executeQuery(sql);
             System.out.println("Group Name");
             //will print the group name and its rows
-            while(rs.next()){
+            while (rs.next()) {
                 groupName = rs.getString("groupName");
                 System.out.println(rs.getRow() + ") " + groupName);
-            }                        
+            }
         } catch (SQLException ex) {
             System.err.println(ex);
         }
     }
+
     /**
-     * displayAllInfo will display all the information based on the
-     * given groupName given and the books associated with them
+     * displayAllInfo will display all the information based on the given
+     * groupName given and the books associated with them
+     *
      * @param conn
-     * @param gName String, group name to display the information 
+     * @param gName String, group name to display the information
      */
-    public static void displayAllInfo(Connection conn, String gName){
+    public static void displayAllInfo(Connection conn, String gName) {
         String sql = "SELECT * FROM writinggroups NATURAL JOIN books WHERE groupname = ?";
-        ResultSet rs = null;        
-        System.out.println("\nRetrieving all info for " + gName);        
+        ResultSet rs = null;
+        System.out.println("\nRetrieving all info for " + gName);
         String groupname, headwriter, yearformed, subject, booktitle, publishername;
         int yearpublished = 0, numberofpages = 0;
-        
-        try(PreparedStatement stmt = conn.prepareStatement(sql, 
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql,
                 ResultSet.TYPE_SCROLL_INSENSITIVE,
-                ResultSet.CONCUR_READ_ONLY);){
-                stmt.setString(1, gName);            
+                ResultSet.CONCUR_READ_ONLY);) {
+            stmt.setString(1, gName);
             rs = stmt.executeQuery();
-            
-            System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n",
+
+            if (!rs.next()) {
+                System.err.println("NO SUCH GROUP IN DATABASE!");
+            } else {
+                rs.beforeFirst();
+
+                System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n",
                         "Group Name", "Head Writer", "Year Formed",
                         "Subject", "Book Title", "Publishers Name", "Number of Pages");
-            
-            if(!rs.next()){
-                System.err.println("NO SUCH GROUP IN DATABASE!");
             }
-            else
-                rs.beforeFirst();
             //prints all the information of the writing group
-            while(rs.next()){
+            while (rs.next()) {
                 groupname = rs.getString("groupname");
                 headwriter = rs.getString("headwriter");
                 yearformed = rs.getString("yearformed");
@@ -66,7 +71,7 @@ public class WritingGroups {
                 publishername = rs.getString("publishername");
                 yearpublished = rs.getInt("yearpublished");
                 numberofpages = rs.getInt("numberofpages");
-                
+
                 System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n",
                         groupname, headwriter, yearformed,
                         subject, booktitle, publishername, numberofpages);
@@ -75,8 +80,7 @@ public class WritingGroups {
             System.err.println(ex);
         }
     }
-    
-    
+
     public static boolean checkWriting(Connection conn, String wname) {
         String sql = "SELECT * FROM writinggroups WHERE groupname = ?";
 
@@ -108,4 +112,3 @@ public class WritingGroups {
         }
     }
 }
-  
